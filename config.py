c13913e7e1c342bbf9296fbb5058400449977d1d
akshay310 <70632287+akshay310@users.noreply.github.com>
2025-03-18 10:55:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 Configuration file for PostgreSQL connection.
 """
 
-DB_URL = "jdbc:postgresql://localhost:5432/iowa_liquor_sales"
+DB_URL = "jdbc:postgresql://localhost:5432/records"
 DB_PROPERTIES = {
-    "user": "myuser",
+    "user": "postgres",
     "password": "password",
     "driver": "org.postgresql.Driver"
 }
